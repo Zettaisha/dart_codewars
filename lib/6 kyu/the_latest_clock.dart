25316e138c7ed718966cea2175ebf3d9efaa9d7a
Zettaisha <levvotintsev18@gmail.com>
2024-04-05 18:13:44 +0300

    the latest clock (test passed, attempt not)

--- lib/6 kyu/the_latest_clock.dart
+++ lib/6 kyu/the_latest_clock.dart
@@ -12,10 +12,51 @@
 // https://www.codewars.com/kata/58925dcb71f43f30cd00005f/train/dart
 
 void main() {
-  print(latestClock(1, 9, 8, 3)); // 19:38
-  print(latestClock(9, 1, 2, 5)); // 21:59
+  // print(latestClock(1, 9, 8, 3)); // 19:38
+  // print(latestClock(9, 1, 2, 5)); // 21:59
+  print(latestClock(1, 2, 8, 9)); // 19:28
 }
 
 String latestClock(int a, int b, int c, int d) {
-  return ""; // Code here
+  List<int> digits = [a, b, c, d].toList()..sort();
+  List<int> result = [];
+
+  // first digit of hours is max between 0 and 2
+  for (var digit in digits.reversed) {
+    if (digit == 2) {
+      // Проверяем наличие еще двух подходящих цифр для оставшихся позиций
+      bool hasHourDigits = digits.where((e) => e != digit).any((e) => e <= 3);
+      bool hasMinuteDigits = digits.where((e) => e != digit).any((e) => e <= 5);
+      if (hasHourDigits && hasMinuteDigits) {
+        result.add(digit);
+        digits.remove(digit);
+        break;
+      }
+    } else if (digit < 2) {
+      result.add(digit);
+      digits.remove(digit);
+      break;
+    }
+  }
+
+  // second is max between 0 and 3 if first is 2, else max from 0-9 (because 20-23...)
+  if (result[0] == 2) {
+    int maxDigit = digits.reversed.firstWhere((digit) => digit <= 3);
+    result.add(maxDigit);
+    digits.remove(maxDigit);
+  } else {
+    int maxDigit = digits.reversed.first;
+    result.add(maxDigit);
+    digits.remove(maxDigit);
+  }
+
+  // first digit of minutes is max between 0 and 5
+  int maxMinuteDigit = digits.reversed.firstWhere((digit) => digit <= 5);
+  result.add(maxMinuteDigit);
+  digits.remove(maxMinuteDigit);
+
+  // last one is the last one ;D
+  result.add(digits.first);
+
+  return '${result[0]}${result[1]}:${result[2]}${result[3]}';
 }
